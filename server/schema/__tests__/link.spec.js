83e6d3b800c3155f8fda19ed0126c8bc81f1e29c
Tyler Reckart <tyler.reckart@gmail.com>
2018-06-11 19:32:56 -0400

    Mock test

--- server/schema/__tests__/link.spec.js
+++ server/schema/__tests__/link.spec.js
@@ -1,14 +1,8 @@
 import axios from 'axios';
+import response from './__mocks__/response.json';
 
 describe('The Link query', () => {
-  test('Queries for a link with a given _id attribute',
-  async () => {
-    const response = {
-      _id: '5a667e7fa66a67444e14e278',
-      url: 'https://google.com/',
-      description: 'Google',
-    };
-
+  test('Queries for a link with a given _id attribute', async () => {
     await axios({
       url: 'http://localhost:4000/graphql',
       method: 'post',
