a24fcfee85889d26da7f03a5fa0527607975bfd8
Tyler Reckart <tyler.reckart@gmail.com>
2018-10-31 20:06:57 -0400

    Hook up link submission

--- client/src/components/Modal/Destroy/index.js
+++ client/src/components/Modal/Destroy/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
 import Modal from '../';
+
+library.add(faTrash);
 
 const ConfirmationModal = () => (
   <Modal headerText="Are you absolutely sure?">
     <p>This action <strong>cannot</strong> be undone.</p>
     <div className="button-group">
       <button className="modal-action-button cancel">Cancel</button>
-      <button className="modal-action-button destroy">Delete</button>
+      <button className="modal-action-button destroy"><FontAwesomeIcon className="button-icon" icon="trash" />Delete</button>
     </div>
   </Modal>
 );
--- client/src/components/Modal/Submit/index.js
+++ client/src/components/Modal/Submit/index.js
@@ -1,36 +1,56 @@
-import React, { Component } from 'react';
-import Modal from '../';
+import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+
+import Modal from "../";
+
+const SUBMIT_LINK_MUTATION = gql`
+  mutation SubmitLink($url: String!) {
+    createLink(url: $url) {
+      _id
+      author {
+        _id
+        username
+      }
+      created_at
+      comments {
+        _id
+      }
+      description
+      score
+      url
+    }
+  }
+`;
 
 class SubmitForm extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      url: '',
-      isValid: false,
-    }
+      url: "",
+      isValid: false
+    };
 
     this.onChange = this.onChange.bind(this);
   }
 
   onChange(e) {
     const { url } = this.state;
-    const isValid = this.validateUrl(url);
+    const isValid = /[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/.test(
+      url
+    );
 
     if (isValid) {
       return this.setState({
         url: e.target.value,
-        isValid,
+        isValid
       });
     }
 
     return this.setState({
-      url: e.target.value,
+      url: e.target.value
     });
-  }
-
-  validateUrl(string) {
-    return /[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/.test(string);
   }
 
   render() {
@@ -39,18 +59,38 @@ class SubmitForm extends Component {
     return (
       <div className="submit-form-wrapper flex justify-content-center">
         <div className="submit-form-content">
-          <form className="justify-content-between align-items-center">
-            <input type="text" value={url} onChange={e => this.onChange(e)} placeholder="Enter a URL..." />
-            {(url.length > 0) && !isValid ? (
-              <div className="validation-error">
-                <p>Please enter a valid URL</p>
-              </div>
-            ) : null}
-            <div className="submit-form-action-area">
-              <button className={`modal-action-button submit-button-${(url.length > 0) && isValid ? 'active' : 'disabled'}`}>Submit</button>
-              <span className="instructions">Or, press <code>enter</code> to submit</span>
-            </div>
-          </form>
+          <Mutation mutation={SUBMIT_LINK_MUTATION} variables={{ url }}>
+            {mutate => (
+              <form
+                className="justify-content-between align-items-center"
+                onSubmit={mutate}
+              >
+                <input
+                  type="text"
+                  value={url}
+                  onChange={e => this.onChange(e)}
+                  placeholder="Enter a URL..."
+                />
+                {url.length > 0 && !isValid ? (
+                  <div className="validation-error">
+                    <p>Please enter a valid URL</p>
+                  </div>
+                ) : null}
+                <div className="submit-form-action-area">
+                  <input
+                    type="submit"
+                    className={`modal-action-button submit-button-${
+                      url.length > 0 && isValid ? "active" : "disabled"
+                    }`}
+                    value="Submit"
+                  />
+                  <span className="instructions">
+                    Or, press <code>enter</code> to submit
+                  </span>
+                </div>
+              </form>
+            )}
+          </Mutation>
         </div>
       </div>
     );
@@ -59,7 +99,7 @@ class SubmitForm extends Component {
 
 const SubmitModal = () => (
   <Modal headerText="Submit a new link">
-      <SubmitForm />
+    <SubmitForm />
   </Modal>
 );
 
