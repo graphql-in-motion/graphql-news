eb46a0e55dedede33eed9b8e86392f05fcf951b5
Tyler Reckart <tyler.reckart@gmail.com>
2018-12-22 00:02:02 -0500

    Continute to build out client features

--- client/src/components/FilterBar/index.js
+++ client/src/components/FilterBar/index.js
@@ -26,34 +26,50 @@ export default class FilterBar extends Component {
         >
           <FontAwesomeIcon className="fa-icon" icon={icon} />{sortBy}
         </button>
-        {showSortMenu && (
-          <div className="filter-menu">
-            <button
-              className={`filter-option ${sortBy === 'new' ? 'active' : ''}`}
-              onClick={() =>
-                this.setState({ sortBy: 'new', icon: 'certificate' })
-              }
-            >
-              <FontAwesomeIcon className="fa-icon" icon="certificate" />New
-            </button>
-            <button
-              className={`filter-option ${sortBy === 'top' ? 'active' : ''}`}
-              onClick={() =>
-                this.setState({ sortBy: 'top', icon: 'fire' })
-              }
-            >
-              <FontAwesomeIcon className="fa-icon" icon="fire" />Top
-            </button>
-            <button
-              className={`filter-option ${sortBy === 'comments' ? 'active' : ''}`}
-              onClick={() =>
-                this.setState({ sortBy: 'comments', icon: 'comment' })
-              }
-            >
-              <FontAwesomeIcon className="fa-icon" icon="comment" />Comments
-            </button>
-          </div>
-        )}
+        <div className="popup-wrapper">
+          {showSortMenu && (
+            <div className="filter-menu">
+              <div className="menu-arrow">
+                <svg
+                  aria-label="up-arrow"
+                  className="up-arrow"
+                  height="7"
+                  id="svg-up-arrow"
+                  role="img"
+                  version="1.1"
+                  viewBox="0 0 11 7"
+                  width="11"
+                >
+                  <path d="m.202 5.715c-.367.417-.217.755.329.755h9.438c.549 0 .702-.33.338-.742l-4.41-4.985c-.363-.41-.947-.412-1.322.013l-4.373 4.96" />
+                </svg>
+              </div>
+              <button
+                className={`filter-option ${sortBy === 'new' ? 'active' : ''}`}
+                onClick={() =>
+                  this.setState({ sortBy: 'new', icon: 'certificate' })
+                }
+              >
+                <FontAwesomeIcon className="fa-icon" icon="certificate" />New
+              </button>
+              <button
+                className={`filter-option ${sortBy === 'top' ? 'active' : ''}`}
+                onClick={() =>
+                  this.setState({ sortBy: 'top', icon: 'fire' })
+                }
+              >
+                <FontAwesomeIcon className="fa-icon" icon="fire" />Top
+              </button>
+              <button
+                className={`filter-option ${sortBy === 'comments' ? 'active' : ''}`}
+                onClick={() =>
+                  this.setState({ sortBy: 'comments', icon: 'comment' })
+                }
+              >
+                <FontAwesomeIcon className="fa-icon" icon="comment" />Comments
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     );
   }
